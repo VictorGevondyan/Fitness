2b3934540cb905f12233da8103b7a31f84341651
AnhaytAnanun <anhaytananun@gmail.com>
2017-03-09 13:18:49 +0400

    birthday setter

--- app/src/main/java/com/flycode/jasonfit/activity/model/User.java
+++ app/src/main/java/com/flycode/jasonfit/activity/model/User.java
@@ -1,5 +1,7 @@
 package com.flycode.jasonfit.activity.model;
 
+import android.content.Context;
+
 import io.t28.shade.annotation.Preferences;
 import io.t28.shade.annotation.Property;
 
@@ -9,11 +11,54 @@ import io.t28.shade.annotation.Property;
 
 @Preferences("com.flycode.jasonfit.user")
 public abstract class User {
+    public static final class GENDER {
+        public static final String MALE = "male";
+        public static final String FEMALE = "female";
+    }
+
+    public static final class LANGUAGE {
+        public static final String ENGLISH = "english";
+        public static final String DEUTSCH = "deutsch";
+    }
+
+    public static final class MEASUREMENTS {
+        public static final String CM = "cm";
+        public static final String KG = "kg";
+        public static final String FOOT = "foor";
+        public static final String POUND = "pount";
+    }
+
+    private static UserPreferences userPreferences;
+
+    public static UserPreferences sharedPreferences(Context context) {
+        if (userPreferences == null) {
+            userPreferences = new UserPreferences(context);
+        }
+
+        return userPreferences;
+    }
 
     @Property(key = "height", defValue = "-1")
     public abstract int height();
 
-    @Property(key = "width", defValue = "-1")
-    public abstract int width();
+    @Property(key = "weight", defValue = "-1")
+    public abstract int weight();
 
+    @Property(key = "birthday", defValue = "768510000000")
+    public abstract long birthday();
+
+    @Property(key = "gender", defValue = GENDER.MALE)
+    public abstract String gender();
+
+    @Property(key = "nutrition", defValue = "vegan")
+    public abstract String nutrition();
+
+    @Property(key = "language", defValue = LANGUAGE.ENGLISH)
+    public abstract String language();
+
+    @Property(key = "heightMeasurement", defValue = MEASUREMENTS.CM)
+    public abstract String heightMeasurement();
+
+    @Property(key = "weightMeasurement", defValue = MEASUREMENTS.KG)
+    public abstract String weightMeasurement();
 }
