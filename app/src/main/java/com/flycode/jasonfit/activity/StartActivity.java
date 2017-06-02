84435e0477251274dc7c582e05d0c4bf59a11f7a
Schumakher <karnav.navasardyan@gmail.com>
2017-06-02 12:39:40 +0000

    checkActiveSubscriptions

--- app/src/main/java/com/flycode/jasonfit/activity/StartActivity.java
+++ app/src/main/java/com/flycode/jasonfit/activity/StartActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.ServiceConnection;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -23,6 +24,8 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -45,8 +48,8 @@ public class StartActivity extends AppCompatActivity {
 
         userPreferences = User.sharedPreferences(this);
 
-        Intent serviceIntent = new Intent("com.flycode.jasonfit.billing.InAppBillingService.BIND");
-        serviceIntent.setPackage("com.flycode.jasonfit");
+        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
         bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
     }
 
@@ -80,11 +83,17 @@ public class StartActivity extends AppCompatActivity {
                 return;
             }
 
+            if (checkForActiveSubscriptions()) {
+                Intent mainActivityIntent = new Intent(this,MainActivity.class);
+                startActivity(mainActivityIntent);
+                return;
+            }
+
             buyItem(skuFromBillingRequest());
 
             Log.i("TAGG", "error, no sku");
 
-        } catch (InterruptedException | JSONException | IntentSender.SendIntentException | RemoteException e) {
+        } catch (InterruptedException | JSONException | IntentSender.SendIntentException | RemoteException | ExecutionException e) {
             e.printStackTrace();
         }
 
@@ -105,7 +114,7 @@ public class StartActivity extends AppCompatActivity {
         }
     };
 
-    private String skuFromBillingRequest() throws InterruptedException, JSONException {
+    private String skuFromBillingRequest() throws InterruptedException, JSONException, ExecutionException {
         ArrayList<String> skuList = new ArrayList<>();
         skuList.add("monthlySubscription");
 
@@ -115,36 +124,33 @@ public class StartActivity extends AppCompatActivity {
         //Below We create new thread because as google says ->
         // "calling this method triggers a network request that could block your main thread"
 
-        //In other hand We create class to be able get data from there after running it in other thread
-                //because it is more easier then callback from thread
-        class SkuDetailsRequester implements Runnable {
-            private Bundle skuDetails;
-
+        AsyncTask<Void, Integer, Bundle> task = new AsyncTask<Void, Integer, Bundle>() {
             @Override
-            public void run() {
+            protected Bundle doInBackground(Void... params) {
+
                 try {
-                    skuDetails = inAppBillingService.getSkuDetails(3,
+                    Bundle skuDetails = inAppBillingService.getSkuDetails(3,
                             getPackageName(),
                             "subs",
                             querySkus);
+
+                    return skuDetails;
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
+                return null;
             }
 
-            private Bundle getSkuDetails() {
-                return skuDetails;
+            @Override
+            protected void onPostExecute(Bundle bundle) {
+                super.onPostExecute(bundle);
             }
-        }
+        };
 
-        SkuDetailsRequester skuDetailsRequester = new SkuDetailsRequester();
-        Thread thread = new Thread(skuDetailsRequester);
-        thread.start();
-        thread.join();
+        task.execute();
+        Bundle skuDetails = task.get();
 
         //end of thread hardcore party
-
-        Bundle skuDetails = skuDetailsRequester.getSkuDetails();
 
         int response = skuDetails.getInt("RESPONSE_CODE");
 
@@ -159,6 +165,7 @@ public class StartActivity extends AppCompatActivity {
 
                 if (sku.equals("monthlySubscription")) {
                     premiumUpgradePrice = price;
+                    return sku;
                 }
             }
         }
@@ -169,7 +176,7 @@ public class StartActivity extends AppCompatActivity {
     private void buyItem(String sku) throws IntentSender.SendIntentException, RemoteException {
         Bundle buyIntentBundle = inAppBillingService.getBuyIntent(3, getPackageName(), sku, "subs", null /*developerPayload*/);
 
-        PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
+        PendingIntent pendingIntent = buyIntentBundle.getParcelable("RESPONSE_BUY_INTENT");
 
         startIntentSenderForResult(pendingIntent.getIntentSender(),
                 BUY_REQUEST_CODE,
@@ -188,7 +195,6 @@ public class StartActivity extends AppCompatActivity {
 
         int responseCode = data.getIntExtra("RESPONSE_CODE", 0);
         String purchaseData = data.getStringExtra("INAPP_PURCHASE_DATA");
-        String dataSignature = data.getStringExtra("INAPP_DATA_SIGNATURE");
 
         if (responseCode != RESULT_OK) {
             return;
@@ -206,5 +212,28 @@ public class StartActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+    private boolean checkForActiveSubscriptions() throws RemoteException, JSONException, ExecutionException, InterruptedException {
+
+        Bundle activeSubscriptions = inAppBillingService.getPurchases(3, "com.flycode.jasonfit", "subs", null);
+
+        if (activeSubscriptions.getInt("RESPONSE_CODE") == 0) {
+
+            List<String> responseList = activeSubscriptions.getStringArrayList("INAPP_PURCHASE_DATA_LIST");
+
+            for (String purchaseData : responseList) {
+
+                JSONObject jsonObject = new JSONObject(purchaseData);
+                String productId = jsonObject.getString("productId");
+
+                if (productId.equals(skuFromBillingRequest())) {
+                    return true;
+                }
+            }
+
+        }
+
+        return false;
     }
 }
