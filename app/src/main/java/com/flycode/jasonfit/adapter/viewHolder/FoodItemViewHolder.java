eae29d51de5ed05fe8d628104af96e170124ec93
Schumakher <karnav.navasardyan@gmail.com>
2017-10-31 07:39:04 +0000

    gradle update

--- app/src/main/java/com/flycode/jasonfit/adapter/viewHolder/FoodItemViewHolder.java
+++ app/src/main/java/com/flycode/jasonfit/adapter/viewHolder/FoodItemViewHolder.java
@@ -45,17 +45,11 @@ public class FoodItemViewHolder extends RecyclerView.ViewHolder {
 
     @OnClick(R.id.add_food_button)
     public void onAddButtonClickListener() {
-//        countTextView.setText(String.valueOf(Integer.valueOf(countTextView.getText().toString()) + 1));
         onAddItemClickListener.onAddFoodClick(this);
     }
 
     @OnClick(R.id.remove_food_button)
     public void onRemoveFoodButtonClickListener() {
-//        if (countTextView.getText().toString().equals("0")) {
-//            return;
-//        }
-
-//        countTextView.setText(String.valueOf(Integer.valueOf(countTextView.getText().toString()) - 1));
         onAddItemClickListener.onRemoveFoodClick(this);
     }
 
